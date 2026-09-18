432a0251a844d739a758ee46cbb42371d795546a
Juan Eduardo Ramirez <juaneduardo.ramirez@upr.edu>
2026-09-18 14:24:51 -0400

    adding download script for os installer

---
+++ rumkernel-devel/download_installer_alma9.sh
@@ -0,0 +1,2 @@
+[ -f ./AlmaLinux-9.8-x86_64-boot.iso ] || echo running wget https://mirrors.upr.edu/almalinux/9.8/isos/x86_64/AlmaLinux-9.8-x86_64-boot.iso
+[ -f ./AlmaLinux-9.8-x86_64-boot.iso ] || wget https://mirrors.upr.edu/almalinux/9.8/isos/x86_64/AlmaLinux-9.8-x86_64-boot.iso
---
+++ rumkernel-devel/download_installer_rocky9.sh
@@ -0,0 +1,2 @@
+[ -f ./Rocky-9.8-x86_64-boot.iso ] || echo running wget https://mirrors.upr.edu/rocky/9.8/isos/x86_64/Rocky-9.8-x86_64-boot.iso
+[ -f ./Rocky-9.8-x86_64-boot.iso ] || wget https://mirrors.upr.edu/rocky/9.8/isos/x86_64/Rocky-9.8-x86_64-boot.iso
