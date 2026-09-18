27112718923481a8ca1daf8a62c05cd60476a302
Juan Eduardo Ramirez <juaneduardo.ramirez@upr.edu>
2026-09-18 14:59:40 -0400

    extract iso boot installer script

---
+++ rumkernel-devel/extract_installer_rocky9.sh
@@ -0,0 +1,8 @@
+ossufix=rocky
+osname=Rocky
+
+#create working directory
+[ -d isoboot_source_$ossufix ]||mkdir -p isoboot_source_$ossufix
+
+# Extract the official AlmaLinux ISO contents into workging directory
+xorriso -osirrox on -indev ./${osname}-9.8-x86_64-boot.iso -extract / isoboot_source_$ossufix/
