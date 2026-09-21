08fc85357d2c840316e0451237ba7fd139cad148
Juan Eduardo Ramirez <juaneduardo.ramirez@upr.edu>
2026-09-21 09:50:58 -0400

    fix create_product_img script

--- rumkernel-devel/create_product_img.sh
+++ rumkernel-devel/create_product_img.sh
@@ -2,5 +2,5 @@
 rm product.img
 mksquashfs addon-work/product/ product.img -comp xz
 
-[ -d isoboot_source_alma ]cp -p product.img isoboot_source_alma/images/
-[ -d isoboot_source_rocky ]cp -p product.img isoboot_source_rocky/images/
+[ -d isoboot_source_alma ]&&cp -p product.img isoboot_source_alma/images/
+[ -d isoboot_source_rocky ]&&cp -p product.img isoboot_source_rocky/images/
